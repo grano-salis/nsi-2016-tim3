0fff3d80688c2cbd6ec01dbf8860171f2e1ba4a5
AdnaDurakovic <adurakovic4@etf.unsa.ba>
2016-12-14 21:00:04 +0100

    webapp - ispravka

--- web/app/js/app.js
+++ web/app/js/app.js
@@ -6,6 +6,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('nsi.charlie', ['nsi.charlie.services', 'nsi.charlie.controllers']).
 config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/componentDetails', {templateUrl: 'partials/componentDetailsPartial.html', controller: 'ComponentCtrl'});
+    $routeProvider.when('/componentDetails', {templateUrl: './partials/componentDetailsPartial.html', controller: 'ComponentCtrl'});
     $routeProvider.otherwise({redirectTo: '/componentDetails'});
 }]);
